f54d671529bb01ece19adf7303b6629a07f68ca3
Isaac David Sánchez Sánchez <158796340+IsaacDev2024@users.noreply.github.com>
2026-01-20 23:39:39 -0500

    Add English and Spanish strings

---
+++ lang/en/local_alpy_toolkit.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * English language strings for local_alpy_toolkit.
+ *
+ * @package    local_alpy_toolkit
+ * @copyright  2026 SAVIO - Sistema de Aprendizaje Virtual Interactivo (UTB)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Alpy Toolkit';
+$string['alpy_resource_type'] = 'Alpy Resource Type';
+$string['alpy_resource_type_help'] = 'Select the resource type for the Alpy Model. This will automatically assign the corresponding icon and learning styles.';
+$string['no_specific_type'] = 'Default (No specific type)';
+
+// Resource Types
+$string['resourcetype_map'] = 'Concept Map';
+$string['resourcetype_diagram'] = 'Diagram';
+$string['resourcetype_reading'] = 'Reading';
+$string['resourcetype_audio'] = 'Audio';
+$string['resourcetype_infographic'] = 'Infographic';
+$string['resourcetype_videotutorial'] = 'Video Tutorial';
+$string['resourcetype_conference'] = 'Video Conference';
+$string['resourcetype_animation'] = 'Animation';
+$string['resourcetype_simulation'] = 'Simulation';
+$string['resourcetype_presentation'] = 'Presentation';
+$string['resourcetype_journal'] = 'Journal';
+$string['resourcetype_research'] = 'Research';
+$string['resourcetype_coding'] = 'Coding Exercise';
+$string['resourcetype_debate'] = 'Debate/Forum';
+$string['resourcetype_project'] = 'Project';
+$string['resourcetype_written'] = 'Written Assignment';
+$string['resourcetype_questionnaire'] = 'Questionnaire';
+$string['resourcetype_quiz'] = 'Quiz/Test';
---
+++ lang/es/local_alpy_toolkit.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Spanish language strings for local_alpy_toolkit.
+ *
+ * @package    local_alpy_toolkit
+ * @copyright  2026 SAVIO - Sistema de Aprendizaje Virtual Interactivo (UTB)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Herramientas Alpy';
+$string['alpy_resource_type'] = 'Tipo de Recurso Alpy';
+$string['alpy_resource_type_help'] = 'Selecciona el tipo de recurso según el Modelo Alpy. Esto asignará automáticamente el icono correspondiente y los estilos de aprendizaje.';
+$string['no_specific_type'] = 'Por defecto (Sin tipo específico)';
+
+// Tipos de Recursos
+$string['resourcetype_map'] = 'Mapa Conceptual';
+$string['resourcetype_diagram'] = 'Diagrama';
+$string['resourcetype_reading'] = 'Lectura';
+$string['resourcetype_audio'] = 'Audio';
+$string['resourcetype_infographic'] = 'Infografía';
+$string['resourcetype_videotutorial'] = 'Videotutorial';
+$string['resourcetype_conference'] = 'Videoconferencia';
+$string['resourcetype_animation'] = 'Animación';
+$string['resourcetype_simulation'] = 'Simulación';
+$string['resourcetype_presentation'] = 'Presentación';
+$string['resourcetype_journal'] = 'Diario';
+$string['resourcetype_research'] = 'Búsqueda';
+$string['resourcetype_coding'] = 'Ejercicio de Código';
+$string['resourcetype_debate'] = 'Debate/Foro';
+$string['resourcetype_project'] = 'Proyecto';
+$string['resourcetype_written'] = 'Trabajo Escrito';
+$string['resourcetype_questionnaire'] = 'Cuestionario';
+$string['resourcetype_quiz'] = 'Examen/Quiz';
